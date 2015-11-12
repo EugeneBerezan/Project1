b6f3055caeef9adc1986d0daf81e47e7adc9723e
Eugene Berezan <ievgenii.berezan@gmail.com>
2015-11-12 22:51:52 +0200

    Driver is here

--- IdeaProjects/src/Driver/WebDriverManager.java
+++ IdeaProjects/src/Driver/WebDriverManager.java
@@ -3,9 +3,7 @@ package Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 import java.util.concurrent.TimeUnit;
-
 
 public class WebDriverManager {
 
